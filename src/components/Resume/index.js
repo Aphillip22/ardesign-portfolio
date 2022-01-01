a990ef14d3c1dc8203bbadd23e2bffdef5d0b320
Aphillip22 <amber.rain.phillips@gmail.com>
2021-12-31 22:04:18 -0700

    set up component files for Resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section className="my-5 flex-row resume">
+        
+        </section>
+    );
+}
+
+export default Resume;
